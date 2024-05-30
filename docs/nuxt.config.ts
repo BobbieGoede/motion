e363029f1e85f59f882acdc472863a86c770d27f
Bobbie Goede <bobbiegoede@gmail.com>
2024-05-30 14:57:50 +0200

    docs: fix build failing due to dependency conflict

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -2,6 +2,11 @@ import { resolve } from 'node:path'
 
 export default defineNuxtConfig({
   theme: '@nuxt-themes/docus',
+  vite: {
+    optimizeDeps: {
+      include: ['micromark']
+    }
+  },
   alias: {
     '@vueuse/motion': resolve(__dirname, '../src/index.ts'),
     '@vueuse/motion/nuxt': resolve(__dirname, '../src/nuxt/module.ts'),
